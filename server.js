7974bf1f58252e939992d9905fae0ac0d1cfaacb
Nicola D'Oronzo <nicoladoronzo95@gmail.com>
2019-09-15 16:31:30 +0300

    unsplash and api keys hidden

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const request = require('request');
+const Unsplash = require('unsplash-js').default;
+
+global.fetch = require('node-fetch');
+
+const unsplash = new Unsplash({
+  applicationId: process.env.UNSPLASH_API_KEY,
+});
+
 
 const app = express();
 
-const URL = 'https://translate.yandex.net/api/v1.5/tr.json';
-const key = 'trnsl.1.1.20190708T121747Z.01ac6564636ef01e.c1f2621aa834e2ab8d58b26e6d9402ebe9e3aeec';
+const YANDEX_TRANSLATE_URL = 'https://translate.yandex.net/api/v1.5/tr.json';
+const translateKey = process.env.YANDEX_TRANSLATE_API_KEY;
 
 app.use(express.json());
 app.use(cors());
 
 
-app.post('/', (req, res) => {
+console.log(process.env);
+
+app.post('/translate', (req, res) => {
   const options = {
     method: 'POST',
-    url: `${URL}/translate`,
+    url: `${YANDEX_TRANSLATE_URL}/translate`,
     qs: {
-      key,
+      key: translateKey,
       lang: req.body.lang,
     },
     headers: {
@@ -34,6 +45,13 @@ app.post('/', (req, res) => {
     res.send(body);
   });
 });
+
+app.post('/searchphoto', (req, res) => {
+  console.log(req.body.data)
+  unsplash.search.photos(req.body.data)
+    .then(data => data.json())
+    .then(data => res.send(data))
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => { console.log(`listening on port ${port}`); });
